33e8970fd4db05e57271a83cf778072a1304c38c
Pango-mac <cohena100@gmail.com>
2018-06-22 16:09:12 +0300

    added handle bars helpers

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   // res.send({
@@ -15,15 +23,13 @@ app.get('/', (req, res) => {
   // });
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome to my web site'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
